7a0ed3df5f0eb15f75a0a273c878ceb5e116f382
Daniela <goulartsdaniela@gmail.com>
2022-03-25 22:57:01 -0200

    adc rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+
+  {path:'', redirectTo:'login', pathMatch: 'full'},
+
+  {path:'login', component: LoginComponent},
+  {path:'cadastrar', component: CadastrarComponent},
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
