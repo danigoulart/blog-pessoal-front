0b6f7f213fd9e45153241141632370ff8085f5c5
Daniela <goulartsdaniela@gmail.com>
2022-04-01 22:13:08 -0300

    Consumindo API e Segurança

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
 
 @Component({
-  selector: 'app-root',  // caminho do seletor 
+  selector: 'app-root',  // caminho do seletor
   templateUrl: './app.component.html', //template
   styleUrls: ['./app.component.css'] // estilo css
 })
 export class AppComponent {
-  title = 'BlogPessoal';
+  title = 'SpaceCatBlog';
+
+  constructor(
+    public auth: AuthService
+
+  ){}
 }
